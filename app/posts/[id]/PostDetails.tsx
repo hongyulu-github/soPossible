22ce46b627ff0d195b06d821523cb01752f183e0
Hongyu Lu <hongyu.lu_cn@outlook.com>
2025-03-06 17:50:47 +0100

    add images

--- app/posts/[id]/PostDetails.tsx
+++ app/posts/[id]/PostDetails.tsx
@@ -1,18 +1,41 @@
+import { getTimeSince } from "@/app/utils";
+import { auth } from "@/auth";
+import { prisma } from "@/prisma/client";
 import { Post } from "@prisma/client";
-import { Card, Flex, Heading } from "@radix-ui/themes";
-import React from "react";
-import Markdown from "react-markdown";
+import { StarIcon, TrashIcon } from "@radix-ui/react-icons";
+import { Avatar, Box, Flex, Strong, Text } from "@radix-ui/themes";
+import Image from "next/image";
 
-const PostDetails = ({ post }: { post: Post }) => {
+const PostDetails = async ({ post }: { post: Post }) => {
+  const user = await prisma.user.findUnique({ where: { id: post.userId } });
+  const session = await auth();
+  const isMyPost = user?.email === session?.user?.email;
   return (
-    <Flex direction={"column"} gap={"3"}>
-      <Heading>{post.title}</Heading>
-      <Flex gap={"3"} align="center">
-        <p>{post.createdAt.toDateString()}</p>
+    <Flex direction={"column"} gap={"3"} width={"400px"}>
+      <Flex justify={"between"} align={"center"}>
+        <Flex gap={"2"}>
+          <Avatar
+            fallback={user!.name!}
+            src={user!.image!}
+            radius="full"
+            size={"2"}
+          />
+          <Text>{user!.name}</Text>
+          <Text>{getTimeSince(post.createdAt)}</Text>
+        </Flex>
+        {isMyPost ? <TrashIcon /> : <StarIcon />}
       </Flex>
-      <Card>
-        <Markdown>{post.description}</Markdown>
-      </Card>
+      <Box position={"relative"} width={"400px"} height={"400px"}>
+        <Image
+          src={post.image!}
+          alt={post.title}
+          fill={true}
+          style={{ objectFit: "cover", borderRadius: "10px" }}
+        />
+      </Box>
+      <Text as="p" size="3">
+        <Strong>{post.title}</Strong> {post.description}
+      </Text>
     </Flex>
   );
 };
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,23 +1,8 @@
-import { auth } from "@/auth";
 import { prisma } from "@/prisma/client";
-import {
-  Avatar,
-  Box,
-  Button,
-  Flex,
-  Grid,
-  Separator,
-  Strong,
-  Text,
-} from "@radix-ui/themes";
+import { Flex, Separator } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import DeleteIssueBtn from "./DeleteIssueBtn";
-import EditIssueBtn from "./EditIssueBtn";
-import PostDetails from "./PostDetails";
-import Image from "next/image";
-import { StarIcon } from "@radix-ui/react-icons";
 import CommentList from "./CommentList";
-import { getTimeSince } from "@/app/utils";
+import PostDetails from "./PostDetails";
 
 interface Props {
   params: { id: string };
@@ -32,38 +17,10 @@ const PageDetailPage = async ({ params }: Props) => {
   const postId = params.id;
   const post = await fetchPost(postId);
   if (!post) notFound();
-  const session = await auth();
-
-  const user = await prisma.user.findUnique({ where: { id: post.userId } });
 
   return (
     <Flex gap={"5"} height={"max-content"}>
-      <Flex direction={"column"} gap={"3"} width={"400px"}>
-        <Flex justify={"between"} align={"center"}>
-          <Flex gap={"2"}>
-            <Avatar
-              fallback={user!.name!}
-              src={user!.image!}
-              radius="full"
-              size={"2"}
-            />
-            <Text>{user!.name}</Text>
-            <Text>{getTimeSince(post.createdAt)}</Text>
-          </Flex>
-          <StarIcon />
-        </Flex>
-        <Box position={"relative"} width={"400px"} height={"400px"}>
-          <Image
-            src={post.image!}
-            alt={post.title}
-            fill={true}
-            style={{ objectFit: "cover", borderRadius: "10px" }}
-          />
-        </Box>
-        <Text as="p" size="3">
-          <Strong>{post.title}</Strong> {post.description}
-        </Text>
-      </Flex>
+      <PostDetails post={post} />
       <Separator orientation="vertical" size="3" />
       <Flex direction={"column"}>
         <CommentList post={post} />
